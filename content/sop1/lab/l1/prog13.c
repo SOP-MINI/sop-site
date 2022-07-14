d99628a264d95ea08fd8f744e3d22dc652b3e5b4
Mateusz Majewski <metiulekm@gmail.com>
2022-07-14 16:57:03 +0200

    Fix issues

--- content/sop1/lab/l1/prog13.c
+++ content/sop1/lab/l1/prog13.c
@@ -6,7 +6,7 @@ int main()
 {
     for (int i = 0; i < 15; ++i) {
         // Output the iteration number and then sleep 1 second.
-        fprintf(stderr, "%d", i);
+        printf("%d", i);
         sleep(1);
     }
     return EXIT_SUCCESS;
