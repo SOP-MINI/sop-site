8ec03d9dc14a317a16d9b746694eaa9694bd766c
Szymon Zyguła <szymon@zygula.eu>
2023-10-03 19:26:20 +0200

    Run clang-format on L1 source files

--- content/sop1/lab/l1/prog13.c
+++ content/sop1/lab/l1/prog13.c
@@ -4,10 +4,10 @@
 
 int main()
 {
-    for (int i = 0; i < 15; ++i) {
-        // Output the iteration number and then sleep 1 second.
-        printf("%d", i);
-        sleep(1);
-    }
-    return EXIT_SUCCESS;
+	for (int i = 0; i < 15; ++i) {
+		// Output the iteration number and then sleep 1 second.
+		printf("%d", i);
+		sleep(1);
+	}
+	return EXIT_SUCCESS;
 }
